23a77d6000d6a0d5200923a475766db957d6f0f0
Tarak <tarak@Taraks-MacBook-Pro.local>
2024-12-31 00:19:25 +0530

    support for custom delimiters tests

--- functions/stringCalculator.js
+++ functions/stringCalculator.js
@@ -1,9 +1,16 @@
 function add(numbers) {
     if (numbers === "") return 0;
-    const nums = numbers.split(/[\n,]/).map(Number);
+  
+    let delimiter = /[\n,]/;
+    if (numbers.startsWith("//")) {
+      const parts = numbers.split("\n");
+      delimiter = new RegExp(parts[0][2]); // Extract custom delimiter
+      numbers = parts[1];
+    }
+  
+    const nums = numbers.split(delimiter).map(Number);
     return nums.reduce((sum, num) => sum + num, 0);
   }
-  
   
   
   
