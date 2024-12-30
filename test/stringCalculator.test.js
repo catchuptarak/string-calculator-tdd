b2bcf0d806591589cfc3ed280ecf7764e8f4912b
Tarak <tarak@Taraks-MacBook-Pro.local>
2024-12-31 00:15:38 +0530

    tesy single number

--- test/stringCalculator.test.js
+++ test/stringCalculator.test.js
@@ -3,3 +3,8 @@ const add = require('../functions/stringCalculator');
 test('returns 0 for an empty string', () => {
   expect(add("")).toBe(0);
 });
+
+test('returns the number for a single input', () => {
+    expect(add("1")).toBe(1);
+  });
+  
