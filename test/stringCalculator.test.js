fa244470a9a087ec034228b3cb2f0495290121b6
Tarak <tarak@Taraks-MacBook-Pro.local>
2024-12-31 00:17:39 +0530

    multiple numbers

--- test/stringCalculator.test.js
+++ test/stringCalculator.test.js
@@ -1,14 +1,17 @@
-const add = require('../functions/stringCalculator');
+const add = require("../functions/stringCalculator");
 
-test('returns 0 for an empty string', () => {
+test("returns 0 for an empty string", () => {
   expect(add("")).toBe(0);
 });
 
-test('returns the number for a single input', () => {
-    expect(add("1")).toBe(1);
-  });
+test("returns the number for a single input", () => {
+  expect(add("1")).toBe(1);
+});
 
-  test('returns the sum of two numbers', () => {
-    expect(add("1,2")).toBe(3);
-  });
-  
+test("returns the sum of two numbers", () => {
+  expect(add("1,2")).toBe(3);
+});
+
+test("returns the sum of multiple numbers", () => {
+  expect(add("1,2,3,4")).toBe(10);
+});
