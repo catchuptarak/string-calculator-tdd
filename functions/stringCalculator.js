7e7c9c5b1a6bef026725bb8b9b826fc94244ccc5
Tarak <tarak@Taraks-MacBook-Pro.local>
2024-12-31 00:20:53 +0530

    negative numbers tests

--- functions/stringCalculator.js
+++ functions/stringCalculator.js
@@ -4,13 +4,19 @@ function add(numbers) {
     let delimiter = /[\n,]/;
     if (numbers.startsWith("//")) {
       const parts = numbers.split("\n");
-      delimiter = new RegExp(parts[0][2]); // Extract custom delimiter
+      delimiter = new RegExp(parts[0][2]);
       numbers = parts[1];
     }
   
     const nums = numbers.split(delimiter).map(Number);
+    const negatives = nums.filter(num => num < 0);
+    if (negatives.length > 0) {
+      throw new Error(`negative numbers not allowed ${negatives.join(",")}`);
+    }
+  
     return nums.reduce((sum, num) => sum + num, 0);
   }
+  
   
   
   
--- test/stringCalculator.test.js
+++ test/stringCalculator.test.js
@@ -20,7 +20,10 @@ test("handles new lines between numbers", () => {
   expect(add("1\n2,3")).toBe(6);
 });
 
-test('supports custom delimiters', () => {
-    expect(add("//;\n1;2")).toBe(3);
-  });
-  
+test("supports custom delimiters", () => {
+  expect(add("//;\n1;2")).toBe(3);
+});
+
+test("throws an exception for negative numbers", () => {
+  expect(() => add("1,-2,3,-4")).toThrow("negative numbers not allowed -2,-4");
+});
